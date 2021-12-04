7cb16b47726d6095c909571a7c69e11e2e701a5d
JoseGomezCereto <trabajo.jgomez@gmail.com>
2021-12-04 01:23:07 +0100

    Aplicación ordenada de forma lógica, archivos iniciales creados, view controller funcionando. Interfaz visual creada con botones y stacks.

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Jose Maria Gomez Cereto on 3/12/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
